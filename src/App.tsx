8f6494791411fac9a77ad369667a25cb24bb50bf
SongChiYoung <ees@ees.guru>
2024-04-28 20:16:21 +0900

    FIX: now start button is work

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,22 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import './App.css';
 import { AppProvider } from './components/AppContext';
 import { Question, questions } from '@/constants'
 
 function App() {
-  const ID_FIRST  = 1;
-  const ID_LAST   = 20;
-
-  const [step, setStep] = useState('1');
-  const [undones, setUndones] = useState([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  ]);
-
-  /**
-   * Get undone id in random.
-   */
-  function getUndoneRandom(): number {
-    if (undones.length === 0) {
-      return 0;
-    }
-
-    let rand = 0;
-    do {
-      rand = Math.floor(Math.random() * ID_LAST) + ID_FIRST;
-    } while (!undones.includes(rand));
-
-    return rand;
-  }
-
-  function getQuestion(id: number): Question {
-    let ret = questions.find(val => {
-      return val.id === id;
-    });
-
-    return ret as Question;
-  }
+  const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 함수를 가져옵니다.
+  
+  const handleStartClick = () => {
+    navigate('/random-list'); // 'random-list' 경로로 이동
+  };
 
   return (
   <AppProvider>
     <div className="App">
       <div>당신에게 가장 잘 맞는 리눅스 배포판은?</div>
-      <button>시작하기</button>
+      <button onClick={handleStartClick}>시작하기</button>
     </div>
   </AppProvider>
   );
