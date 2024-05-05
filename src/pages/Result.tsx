ce1dae6bccdc5934ba6a61fba16d7f786e116f21
SongChiYoung <ees@ees.guru>
2024-05-05 20:59:27 +0900

    FEAT: Add redo button

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom'
 import { Result, results } from '@/constants'
+import { useAppContext } from '@/components/AppContext';
 
 
 const ResultPage = () => {
   const params = useParams();
+  const { setNumbers, setSelectedAnswers, setCurrent } = useAppContext();
+  const navigate = useNavigate();
+
+  const clearLocalStorage = () => {
+    //clear local storage
+    localStorage.removeItem('randomNumbers');
+    localStorage.removeItem('selectedAnswers');
+    localStorage.removeItem('current');
+
+    //clear states
+    setNumbers([]);
+    setSelectedAnswers([]);
+    setCurrent(0);
+
+    //navigate to home
+    navigate('/');
+  }
+
   const result = results[params.id as keyof Result];
   return (
     <div
@@ -15,6 +35,8 @@ const ResultPage = () => {
       <div>당신에게 어울리는 배포판은 {result.name} 입니다.</div>
       <div>"이미지"</div>
       <div>{result.desc}</div>
+
+      <button onClick={clearLocalStorage}>다시하기</button>
     </div>
   );
 }
