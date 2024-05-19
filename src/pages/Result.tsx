22090d8671a5e6a396eb6bd383e8a914c38078c0
EeS <ees@ees.guru>
2024-05-19 15:09:40 +0900

    FEAT: designed result page (#15)

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Result.scss'
 import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom'
 import { Result, results } from '@/constants'
@@ -30,13 +31,29 @@ const ResultPage = () => {
     <div
       className="result-page"
     >
-      <div>결과는?</div>
-      <div>당신은 {params.id} 유형입니다!</div>
-      <div>당신에게 어울리는 배포판은 {result.name} 입니다.</div>
-      <div>"이미지"</div>
-      <div>{result.desc}</div>
+      <div className="result-div">
+        <div className='result-image-div'>
+          <img className="result-image" src={process.env.PUBLIC_URL + "/img/" + params.id + ".jpg" } alt="logo" />
+        </div>
+        <div className='result-desc-div'>{result.desc}</div>
 
-      <button onClick={clearLocalStorage}>다시하기</button>
+        <div className='result-bottom-div'>
+          <div className='friends-div'>
+            나랑 잘 맞는 배포판<br/>
+            <div className='friends-image-div' onClick={() => window.location.href="./"+result.best}>
+              <img className="friends-image" src={process.env.PUBLIC_URL + "/img/" + result.best + ".jpg" } alt="logo" />
+            </div>
+          </div>
+          <div className='friends-div'>
+            나랑 안맞는 배포판<br/>
+            <div className='friends-image-div' onClick={() => window.location.href="./"+result.worst}>
+              <img className="friends-image" src={process.env.PUBLIC_URL + "/img/" + result.worst + ".jpg" } alt="logo" />
+            </div>
+          </div>
+        </div>
+        <button className='bottom-button' onClick={clearLocalStorage}>다시하기</button>
+      </div>
+
     </div>
   );
 }
