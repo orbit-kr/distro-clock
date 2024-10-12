0ad5e22a250fd68354563f86e86754a6a309f68b
SongChiYoung <ees@ees.guru>
2024-10-12 15:43:04 +0900

    FEAT: github pages deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,56 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom'
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-import App from './App';
-import { AppProvider } from './components/AppContext';
-import QuestionPage from '@/pages/Question'
-import RandomListPage from '@/pages/RandomList'
-import CalcResultPage from '@/pages/CalcResult'
-import ResultPage from '@/pages/Result'
+import App from "./App";
+import { AppProvider } from "./components/AppContext";
+import QuestionPage from "@/pages/Question";
+import RandomListPage from "@/pages/RandomList";
+import CalcResultPage from "@/pages/CalcResult";
+import ResultPage from "@/pages/Result";
 
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/random-list",
+      element: <RandomListPage />,
+    },
+    {
+      path: "/question",
+      element: <QuestionPage />,
+    },
+    {
+      path: "/calc-result",
+      element: <CalcResultPage />,
+    },
+    {
+      path: "/result/:id",
+      element: <ResultPage />,
+    },
+  ],
   {
-    path: '/',
-    element: <App />,
+    basename: "/distro-clock", // 여기서 basename을 지정하여 기본 경로를 추가합니다.
   },
-  {
-    path: '/random-list',
-    element: <RandomListPage />,
-  },
-  {
-    path: '/question',
-    element: <QuestionPage />,
-  },
-  {
-    path: '/calc-result',
-    element: <CalcResultPage />,
-  },
-  {
-    path: '/result/:id',
-    element: <ResultPage />,
-  },
-]);
+);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <RouterProvider router={router}
-      />
+      <RouterProvider router={router} />
     </AppProvider>
     <footer>
       Copyright &copy; 2024 Orbit Research Centre. All rights reserved.
     </footer>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
